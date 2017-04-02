00dc90f7d20697a377e2b51136cc75fdc20bdb82
Unum Sarfraz <anamsarfraz@gmail.com>
2017-04-02 16:46:47 -0700

    Added tab icons for tab layout

--- app/src/main/java/com/codepath/apps/twitter/adapters/TweetsPagerAdapter.java
+++ app/src/main/java/com/codepath/apps/twitter/adapters/TweetsPagerAdapter.java
@@ -4,18 +4,22 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.astuetz.PagerSlidingTabStrip;
+import com.codepath.apps.twitter.R;
 import com.codepath.apps.twitter.fragments.HomeTimelineFragment;
 import com.codepath.apps.twitter.fragments.MentionsTimelineFragment;
 import com.codepath.apps.twitter.fragments.MessagesFragment;
 
 
 // Order and creation of fragments with the page
-public class TweetsPagerAdapter extends SmartFragmentStatePagerAdapter {
+public class TweetsPagerAdapter extends SmartFragmentStatePagerAdapter implements PagerSlidingTabStrip.IconTabProvider{
     final String HOME = "Home";
     final String MENTIONS = "Mentions";
     final String MESSAGES = "Messages";
 
     private String tabTitles [] = {HOME, MENTIONS, MESSAGES};
+    private int icons[] = {R.drawable.ic_twitter_home, R.drawable.ic_twitter_mention, R.drawable.message_default};
+
 
     // Adapter gets the manager insert or remove from the activity
     public TweetsPagerAdapter(FragmentManager fm) {
@@ -37,14 +41,19 @@ public class TweetsPagerAdapter extends SmartFragmentStatePagerAdapter {
     }
 
     // Return the tab title at the top
-    @Override
+    /*@Override
     public CharSequence getPageTitle(int position) {
         return tabTitles[position];
     }
-
+*/
     // Number of fragments to swipe in between
     @Override
     public int getCount() {
         return tabTitles.length;
+    }
+
+    @Override
+    public int getPageIconResId(int position) {
+        return icons[position];
     }
 }
